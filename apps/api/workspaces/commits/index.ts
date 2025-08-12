7fde2f652900ca3c04a3c4ef46264bdb3dcb0c9a
Michael Gearhardt <michael.gearhardt@fathym.com>
2025-08-12 10:16:46 -0600

    feat: add commit status endpoints

---
+++ apps/api/workspaces/commits/index.ts
@@ -0,0 +1,21 @@
+import { EaCRuntimeHandlers } from '@fathym/eac/runtime/pipelines';
+import { loadEaCStewardSvc } from '@fathym/eac/steward/clients';
+import { OpenIndustrialAPIState } from '../../../../src/state/OpenIndustrialAPIState.ts';
+
+export default {
+  async GET(_req, ctx) {
+    try {
+      if (!ctx.State.EaCJWT) {
+        return new Response('Missing EaC authentication.', { status: 500 });
+      }
+
+      const eacSvc = await loadEaCStewardSvc(ctx.State.EaCJWT);
+
+      const stati = await eacSvc.Status.ListStati();
+
+      return Response.json(stati);
+    } catch {
+      return new Response('Failed to retrieve commit statuses.', { status: 500 });
+    }
+  },
+} as EaCRuntimeHandlers<OpenIndustrialAPIState>;
